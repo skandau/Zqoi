eda167cf68d7008bbbe832ccd1357e807b97fe2f
skandau <73341866+skandau@users.noreply.github.com>
2022-11-25 17:11:56 -0800

    Add files via upload

--- zqoi.cpp
+++ zqoi.cpp
@@ -1,31 +1,3 @@
-/* demo1.cpp - Demo file compressor for libzpaq.
-
-Written by Matt Mahoney, Sept. 27, 2010.
-This program is placed in the public domain.
-It is provided "as is" with no warranty.
-
-To use: demo1 command input output
-Commands:
-  1 = compress fast
-  2 = compress medium
-  3 = compress best
-  d = decompress
-
-For example, to compress the file book1:
-
-  demo1 3 book1 book1.zpaq
-  demo1 d book1.zpaq book1
-
-To compile with g++, Borland, or Mars:
-
-  g++ -O2 demo1.cpp libzpaq.cpp -o demo1
-  bcc32 -O -6 demo1.cpp libzpaq.cpp
-  dmc -o -6 demo1.cpp libzpaq.cpp
-
-Use option -DNDEBUG to turn off run time checks for more speed.
-Use other optimization options as appropriate for your platform.
-These are just examples.
-*/
 
 #include <stdio.h>
 #include <stdlib.h>
